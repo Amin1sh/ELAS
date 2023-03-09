cbf7365039377b5124637833f26a6f1a831132f2
Amin1sh <51711712+Amin1sh@users.noreply.github.com>
2023-03-09 21:37:04 +0100

    add get_user and get_user_by_id function in the Smatch project

--- backend/application/resources/smatch/smatch.py
+++ backend/application/resources/smatch/smatch.py
@@ -1,8 +1,26 @@
 from flask import Blueprint
 from orm_interface.base import Session
+from orm_interface.entities.user import User
 
 smatch = Blueprint("smatch", __name__)
 session = Session()
+
+def get_user(username):
+    
+    # cur.execute('SELECT id, username, password FROM users WHERE username = %s', (username,))
+    user = session.query(User).filter(User.email == username).first()
+
+    if user is None:
+        return None
+    
+    return user
+
+def get_user_by_id(id):
+    
+    # cur.execute('SELECT id, username, password FROM users WHERE id = %s', (id,))
+    user = session.query(User).filter(User.id == id).first()
+
+    return user
 
 @smatch.route("/home")
 @smatch.route("/")
